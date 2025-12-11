7f58b1be5933106c5b84d83b8fb2ce225e1d2d19
Samuel Huwiler <samuel@student-net-etx-0481.intern.ethz.ch>
2025-12-11 13:57:44 +0100

    Attempts

--- day_10.py
+++ day_10.py
@@ -3,14 +3,15 @@ from utils.all import *
 from itertools import combinations
 
 rinupt_10 = read_input_line("test_10", sep="\n")
-rinupt_10 = read_input_line(10,sep="\n")
+rinupt_10 = read_input_line(10, sep="\n")
 
 
 def parse_line(line: str) -> list:
     lights = line.split()[0].strip("[]")
 
     buttons = mapl(tuple, map(integers, line.split()[1:-1]))
-    return [lights, buttons]
+    joltage = integers(line.split()[-1])
+    return [lights, buttons, joltage]
 
 
 input_10 = mapl(parse_line, rinupt_10)
@@ -27,7 +28,6 @@ def press(lights_str: str, button: tuple) -> str:
 
 
 def get_min_button_presses(line: list) -> int:
-    on_lights = [i for i, light in enumerate(line[0]) if light == "#"]
     max_presses = len(line[1])+100
 
     for presses in range(1, max_presses):
@@ -41,8 +41,23 @@ def get_min_button_presses(line: list) -> int:
     return -1
 
 
+def get_min_button_presses2(line: list) -> int:
+    max_presses = len(line[1])+100000
+
+    for presses in range(1, max_presses):
+        for button_order in combinations(line[1], r=presses):
+            counter: list = [0 for _ in range(len(line[2]))]
+            for button in button_order:
+                for i in button:
+                    counter[i] += 1
+                if counter == line[2]:
+                    # print(f"Minimum button presses: {presses}")
+                    return presses
+    return -1
+
+
 def part1() -> None:
-    count:int = 0
+    count: int = 0
     for line in input_10:
         res = get_min_button_presses(line)
         count += res
@@ -50,7 +65,11 @@ def part1() -> None:
 
 
 def part2() -> None:
-    pass
+    count: int = 0
+    for line in input_10:
+        res = get_min_button_presses2(line)
+        count += res
+    print(count)
 
 
 def main() -> int:
